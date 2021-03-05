a9fee9bfbbcd8fcd14551fdbfc2f3db85a9fe4e8
Benjamin Liebe <benjamin.liebe@owasp.org>
2021-03-05 21:03:18 +0100

    Generalized Example

--- Simple/Program.fs
+++ Simple/Program.fs
@@ -5,11 +5,13 @@ open Jslinq.Types
 [<Policy>]
 module Policy =
     [<SecT("_^H -> _^H")>]
-    let foo (_ : int) = 1
+    let sourceH (_ : int) = 1
 
-    [<SecT("_ ->^L _")>]
-    let id (s : int) = s
+    [<SecT("_^L -> _")>]
+    let sinkL (s : int) = s
 
 module Application =
-    let bar = id 1
-    //Policy.id 1
+    let constInt = 1
+    let valH = Policy.sourceH 1
+    let computation1 = Policy.sinkL (Policy.sourceH 1) // MatchFailureException when removed
+    let computation2 = Policy.sinkL valH
